6f4b46b27b1aa731bfd07de3bf3058b4f0931bff
hr <ipowerink@outlook.com>
2017-08-13 17:32:37 +0800

    直接插入排序

---
+++ 数据结构/排序/record.py
@@ -0,0 +1,45 @@
+class record:
+    def __init__(self, key=[1, 2, 3, 4, 5, 6]):
+        self.key = [0]
+        self.key.extend(key)
+
+    def show_record(self):
+        for each in self.key[1:]:
+            print(each, end=' ')
+        print('\n')
+
+    # 直接插入排序
+    def insert_sort(self):
+        for i in range(2, self.key.__len__()):
+            self.key[0] = self.key[i]
+            j = i - 1
+
+            while self.key[0] < self.key[j]:  # 寻找插入位置
+                self.key[j + 1] = self.key[j]  # 后移
+                j -= 1
+            self.key[j + 1] = self.key[0]
+
+    # 折半插入排序
+    def binary_sort(self):
+        for i in range(2, self.key.__len__()):
+            self.key[0] = self.key[i]  # 监视哨存放待插入的元素
+
+            left = 1
+            right = i - 1
+
+            while left <= right:  # 寻找插入位置
+                mid = (left + right) // 2
+                if self.key[0] < self.key[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            for j in range(left, i)[::-1]:
+                self.key[j + 1] = self.key[j]
+
+            self.key[mid] = self.key[0]
+
+
+obj = record([2, 1, 3])
+obj.binary_sort()
+obj.show_record()
